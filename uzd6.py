0a6960864525fa8ddfd668bd5d1b3bfb51d366a0
rudiskrahhhh <149486943+rudiskrahhhh@users.noreply.github.com>
2024-09-28 16:27:28 +0300

    Update uzd6.py

---
+++ uzd6.py
@@ -0,0 +1,8 @@
+answer = str(input("do u want the program to repeat??? y/n "))
+
+if answer == "y":
+    answer = str(input("do u want the program to repeat??? y/n "))
+elif answer == "n":
+    print("okay byebye")
+else:
+    print("byebye i dont understand this language")
