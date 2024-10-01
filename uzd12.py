8fd36e40e001609c240cd8c52cc2a021c48bda78
rudiskrahhhh <rudis.kruskops@gmail.com>
2024-10-01 22:46:40 +0300

    Update uzd12.py

--- uzd12.py
+++ uzd12.py
@@ -1,2 +1,9 @@
-for x in "banana":
-  print(x, x)
+divnieks = 2
+
+kapinatajs = int(input("ievadi skaitli kurs kapinas 2 "))
+
+for i in range(kapinatajs):
+
+    divnieks = divnieks*2
+print(divnieks)
+    
