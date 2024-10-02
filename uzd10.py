229f0042153ffa6ba4c11f9c6f8492bc311c3e1a
rudiskrahhhh <rudis.kruskops@gmail.com>
2024-10-02 14:53:02 +0300

    Update uzd10.py

--- uzd10.py
+++ uzd10.py
@@ -1,17 +1,17 @@
 
 
 def fibonaci():
-    x=1
-    y=0
-    z=0
+    x=0
+    y=1
+    z=1
     summa=0
     while z <= int(4000000):
-        for i in range(1, 40):
-            z = x + y
-            x = y
-            y = z
-            print(summa)
-            if z % 2 == 0:
-                summa += z
+        
+        z = x + y
+        x = y
+        y = z
+        print(summa)
+        if z % 2 == 0:
+            summa += z
     print(summa)
 fibonaci()
