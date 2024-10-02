c62e7c60064e8927038be622a8d9b1905331e280
rudiskrahhhh <rudis.kruskops@gmail.com>
2024-10-02 14:53:07 +0300

    Update uzd14.py

---
+++ uzd14.py
@@ -0,0 +1,10 @@
+skaitlis1 = int(input("ievadiet pirmo skaitli "))
+skaitlis2 = int(input("ievadiet otro skaitli "))
+skaitlis3 = int(input("ievadiet treso skaitli "))
+
+if skaitlis1 > skaitlis2 and skaitlis1 > skaitlis3:
+    print(skaitlis1)
+elif skaitlis2 > skaitlis1 and skaitlis2 > skaitlis3:
+    print(skaitlis2)
+elif skaitlis3 > skaitlis1 and skaitlis3 > skaitlis3:
+    print(skaitlis3)
